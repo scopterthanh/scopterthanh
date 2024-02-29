f9ee6549fe36169a53d55d863d4052d6b9b0c22d
scopterthanh <scopterhi@gmail.com>
2024-02-29 12:57:40 +0700

    py

---
+++ q.py
@@ -0,0 +1,5 @@
+print("Đổi tiền tệ bằng code Python")
+
+usd = input()
+vnd = int(usd) * 25
+print(str(usd) + "USD = " + str(vnd) + "k VND ")
---
+++ w.py
@@ -0,0 +1,14 @@
+print("Quy đổi giá trị chéo với nhau")
+
+a = 1
+b = 2
+print("a = " + str(a))
+print("b = " + str(b))
+
+print("Giá trị như trên nhưng hoán đổi cho nhau")
+
+temp = a
+a = b
+b = temp
+print("a = " + str(a))
+print("b = " + str(b))
